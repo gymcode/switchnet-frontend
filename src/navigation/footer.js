079c907af4b69c27c11f565f50358af5833c90c3
Kenneth <kyleabs20@gmail.com>
2020-11-30 22:13:15 -0800

    best song ever

---
+++ src/navigation/footer.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+import "../styles/main.css"
+import "../styles/App.css"
+
+const Footer = ()=>{
+    return(
+        <Fragment>
+            <div className={'footer'}>
+
+            </div>
+        </Fragment>
+    )
+}
