78dcc0b3dc1fb97ecbad25be4d3cd7d3e4515a14
Kenneth <kyleabs20@gmail.com>
2020-11-30 21:46:05 -0800

    working progress

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -48,10 +48,10 @@ const MainSection  = ()=>{
                 {/* pc section */}
                 <div className={'px-24 py-12'}>
                     <div className={'hidden md:block lg:block xl:block rounded'}>
-                        <div className={"mx-24 py-20 grid grid-cols-12 gap-4"}>
-                            <h2 className={"col-span-3 text_default font_default text-4xl ml-16 capitalize font-bold"}>Gadget 4 ur budget</h2>
-                            <div className={"horiLine col-span-1 ml-10"}/>
-                            <p className={'col-span-8 text_size leading-loose'} style={{color: "#8d97a2"}}>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
+                        <div className={"mx-20 py-20 grid grid-cols-12 gap-4"}>
+                            <h2 className={"col-span-4 text_default font_default text-4xl capitalize font-bold"}>get a gadget 4 ur budget</h2>
+                            <div className={"horiLine col-span-1"}/>
+                            <p className={'col-span-7 text_size leading-loose'} style={{color: "#8d97a2"}}>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
                         </div>
                         <div className={"veriLine"}/>
                         <div className={"mx-8 py-16"}>
