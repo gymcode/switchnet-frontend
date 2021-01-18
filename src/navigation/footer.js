d1e16ca9d2b01f0b28868d1f2c5c5f81c609243b
Kenneth <kyleabs20@gmail.com>
2021-01-18 12:49:33 -0800

    la Hustle

--- src/navigation/footer.js
+++ src/navigation/footer.js
@@ -5,8 +5,17 @@ import "../styles/App.css"
 const Footer = ()=>{
     return(
         <Fragment>
-            <div className={'footer'}>
-                sadadaldadandadladlksadnalskdaldanlda
+            <div className={'footer px-32 py-20 text-white'}>
+                <div classname={"flex justify-between items-center w-full"}>
+                    <div>Switchnet</div>
+                    <div className={"text-xs"}>return to top</div>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
             </div>
         </Fragment>
     )
