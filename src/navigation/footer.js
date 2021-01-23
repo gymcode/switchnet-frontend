8f2d80e57e7794438f30317f2d01d05dbda58d52
Kenneth <kyleabs20@gmail.com>
2021-01-22 16:36:10 -0800

    step

--- src/navigation/footer.js
+++ src/navigation/footer.js
@@ -9,21 +9,21 @@ const Footer = ()=>{
             <div className={'footer px-20 text-white'}>
               
                <div className={"h-64 flex flex-col justify-center items-center"}>
-                    <p className={"text-md"}>switchnet solutions</p> 
+                    <p className={"text-md font_Sans"}>switchnet solutions</p> 
                     
-                    <div className={"text-5xl text-white font_Nunito"}>Gadget for your budget</div>
+                    <div className={"text-5xl text-white font-bold font_Nunito"}>Gadget for your budget</div>
                </div>
                <div className={"flex justify-evenly items-center py-8"}>
-                    <div className={"flex flex-row "}>
+                    <div className={"flex flex-row font_Sans"}>
                         <div className={"px-4"}>Terms</div>
                         <div className={"px-4"}>Privacy</div>
                         <div className={"px-4"}>Services</div>
                         <div className={"px-4"}>Contact</div>
                     </div>
-                    <div>
+                    <div className={"font_Sans"}>
                         sdfsdfsdfsdfsdfsdf
                     </div>
-                    <div>
+                    <div className={"font_Sans"}>
                         <p>&copy; 2016 switchnet solution. all rights reserved</p>
                     </div>
                </div>
