f51feb2345ff4c53ff1bb9e2734e136c50812f73
Kenneth <kyleabs20@gmail.com>
2021-01-22 16:25:13 -0800

    beats

--- src/navigation/footer.js
+++ src/navigation/footer.js
@@ -6,20 +6,25 @@ import {BsBoxArrowInUp} from 'react-icons/bs'
 const Footer = ()=>{
     return(
         <Fragment>
-            <div className={'footer px-32 py-10 text-white'}>
+            <div className={'footer px-20 py-10 text-white'}>
               
                <div className={"h-64 flex flex-col justify-center items-center"}>
                     <p className={"text-md"}>switchnet solutions</p> 
                     
-                    <div className={"text-4xl"}>Gadget for your budget</div>
+                    <div className={"text-4xl text-white"}>Gadget for your budget</div>
                </div>
-               <div className={"flex justify-evenly items-center p-8"}>
-                    <div>
-
+               <div className={"flex justify-evenly items-center py-8"}>
+                    <div className={"flex flex-row"}>
+                        <div className={"px-4"}>Terms</div>
+                        <div className={"px-4"}>Privacy</div>
+                        <div className={"px-4"}>Services</div>
+                        <div className={"px-4"}>Contact</div>
                     </div>
-                    <div></div>
                     <div>
-                        <p>&copy; switchnet_solution.com</p>
+                        sdfsdfsdfsdfsdfsdf
+                    </div>
+                    <div>
+                        <p>&copy; 2016 switchnet solution. all rights reserved</p>
                     </div>
                </div>
             </div>
